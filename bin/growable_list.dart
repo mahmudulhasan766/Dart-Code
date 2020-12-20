ccd48ca12fde6babe4d5261d9e9fd7295c4297d1
HaSaN <mahamudul951@gmail.com>
2020-12-20 23:16:58 +0600

    Growable list

---
+++ bin/growable_list.dart
@@ -0,0 +1,10 @@
+void main() {
+  var myGlist = new List();
+  myGlist.add("hasan");
+  myGlist.add("babu");
+  myGlist.add("asif");
+  myGlist.add("sojib");
+  myGlist.add("emon");
+  myGlist.add("joy");
+  print(myGlist);
+}
