7007e27ccd66b1ea62c82e93a1b2d31c30cfb291
HaSaN <mahamudul951@gmail.com>
2020-12-20 23:29:40 +0600

    Growable list.add,list.insert

--- bin/growable_list.dart
+++ bin/growable_list.dart
@@ -6,5 +6,9 @@ void main() {
   myGlist.add("sojib");
   myGlist.add("emon");
   myGlist.add("joy");
+  myGlist.addAll(["hasan", "babu"]);
+  myGlist.insert(5, "mahumd");
+  myGlist.insertAll(6, ["mahumd*", "babu*", "asif*"]);
+
   print(myGlist);
 }
